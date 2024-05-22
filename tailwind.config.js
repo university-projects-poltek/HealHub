7316b8227e5a2938a1ef9becc6af6ea3c297c305
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-05-22 17:14:39 +0700

    chore: setup flowbite and remove daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 export default {
   content: [
-    // You will probably also need those lines
-    "./resources/**/**/*.{js,blade.php}",
-    "./app/View/Components/**/**/*.php",
-    "./app/Livewire/**/**/*.php",
-
-    // Add mary
-    "./vendor/robsontenorio/mary/src/View/Components/**/*.php",
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./node_modules/flowbite/**/*.js"
   ],
-
-  // Add daisyUI
-  plugins: [require("daisyui")],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+      require('flowbite/plugin')
+  ],
 }
+
