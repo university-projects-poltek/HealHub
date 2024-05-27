e28a338d4c456931f0422fc12532071a5a50be18
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-05-27 22:09:12 +0700

    feat: add layouts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>@yield('title')</title>
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+
+<body class="bg-[#F6F7F9]">
+
+  
+    <x-navbar />
+
+    <main class="max-w-6xl mx-auto">
+      @yield('content')
+    </main>
+
+  
+    <x-footer />
+  
+</body>
+
+</html>
