1a19968eed29e15475612546b2a4b5a5eb675472
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-06-06 19:28:20 +0700

    feat(auth): add user registration feature
    - Implemented user registration functionality in AuthController.
    - Added validation for user input during registration.
    - Integrated with the database to save new user information.
    - Included hashing for passwords before saving to the database.
    - Updated routes to include registration endpoint.

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+  public static function index()
+  {
+    return view('register');
+  }
+
+  public static function store(Request $request)
+  {
+    $validatedData = $request->validate([
+      'username' => 'required',
+      'email' => 'required|email:dns|unique:users',
+      'password' => 'required|min:5|max:255',
+    ]);
+
+    $validatedData['role'] = 'buyer';
+    $validatedData['password'] = Hash::make($validatedData['password']);
+
+    $createUser = User::create($validatedData);
+
+    if (!$createUser) {
+      return redirect('/register')->with('failed', 'Registration failed! Please try again');
+    }
+
+    return redirect('/login')->with('success', 'Registration successfull! Please login');
+  }
+}
