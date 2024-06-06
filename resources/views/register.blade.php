0ec00300a5a4965986470c792a0782feb4ca7ada
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-06-06 19:34:10 +0700

    style(blade): add register blade template

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,5 +1,36 @@
-<div class="flex justify-center items-center min-h-screen">
-    <div class="p-5 bg-blue-400">
-        <h1 class="text-2xl font-bold text-white pb-5">Login</h1>
+<html>
+
+<head>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <div class="flex justify-center items-center min-h-screen">
+
+        <form action="{{ route('register') }}" method="POST">
+            @csrf
+            @method('POST')
+            <div class="flex flex-col w-full gap-2">
+                <input type="email" name="email" placeholder="Email" value="{{ old('email')}}">
+                @error('email')
+                    <small>{{ $message }}</small>
+                @enderror
+
+                <input type="text" name="username" placeholder="Username" value="{{ old('username')}}">
+                @error('username')
+                    <small>{{ $message }}</small>
+                @enderror
+
+                <input type="password" name="password" placeholder="password" value="{{ old('password')}}">
+
+                @error('password')
+                    <small>{{ $message }}</small>
+                @enderror
+                <button type="submit">Sign Up</button>
+            </div>
+        </form>
     </div>
-</div>
+
+</body>
+
+</html>
