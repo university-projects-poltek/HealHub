5594750845e52e4d145ab3a253852dccdf5a1a61
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-06-20 20:21:24 +0700

    style: update app layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,30 +1,30 @@
 <html>
 
 <head>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
-    <div class="flex justify-center items-center min-h-screen">
+  <div class="flex justify-center items-center min-h-screen">
 
-        <form action="{{ route('login') }}" method="POST">
-            @csrf
-            @method('POST')
-            <div>
-                <input type="email" name="email" placeholder="Email">
-                @error('email')
-                    <small>{{ $message }}</small>
-                @enderror
+    <form action="{{ route('login') }}" method="POST">
+      @csrf
+      @method('POST')
+      <div>
+        <input type="email" name="email" placeholder="Email">
+        @error('email')
+        <small>{{ $message }}</small>
+        @enderror
 
-                <input type="password" name="password" placeholder="password">
+        <input type="password" name="password" placeholder="password">
 
-                @error('password')
-                    <small>{{ $message }}</small>
-                @enderror
-                <button type="submit">Sign In</button>
-            </div>
-        </form>
-    </div>
+        @error('password')
+        <small>{{ $message }}</small>
+        @enderror
+        <button type="submit">Sign In</button>
+      </div>
+    </form>
+  </div>
 
 </body>
 
