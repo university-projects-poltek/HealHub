f601f40c70de94be9493a867bc6d11698fa92ea1
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-06-20 20:54:52 +0700

    feat: create dashboard layout

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Apotek Cahaya Garden</title>
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+
+<body class="bg-[#F6F7F9]">
+
+    @if (Auth::check() && Auth::user()->role == 'admin' )
+      <x-navbar-admin avatar="{{ Auth::user()->avatar }}" name="{{ Auth::user()->name }}" />
+    @else
+      <x-navbar-user avatar="{{ Auth::user()->avatar }}" name="{{ Auth::user()->name }}" />
+    @endif
+
+    <main class="max-w-6xl mx-auto mt-10 bg-white rounded-[10px] min-h-screen">
+      <section class="px-16 py-8">
+        @yield('content')
+      </section>
+    </main>
+
+</body>
+
+</html>
