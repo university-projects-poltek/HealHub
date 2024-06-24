4b32efa7ea3ee480fc9243753ba2f1b0d6f4e843
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-06-24 14:29:42 +0700

    feat: update routes

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use Illuminate\Support\Facades\Route;
 
 
 // Auth
-Route::get('/auth/google', [SocialController::class, 'getSocialRedirect']);
+Route::get('/auth/google', [SocialController::class, 'getSocialRedirect'])->name('auth.google');
 Route::get('/auth/callback', [SocialController::class, 'getSocialHandle']);
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
@@ -36,22 +36,27 @@ Route::post('/register', [RegisterController::class, 'store']);
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/products/{id}', [ProductController::class, 'show']);
 
-Route::middleware(['auth', 'role:user'])->group(function () {
-  Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-});
 
-Route::middleware(['auth', 'role:admin'])->group(function () {
-  Route::prefix('/dashboard')->group(function () {
-    Route::get('/', [DashboardController::class, 'getAdminDashboard'])->name('admin.dashboard');
-    Route::get('/orders', [OrderController::class, 'getAdminOrder'])->name('admin.orders');
+Route::middleware(['auth'])->group(function () {
 
-    Route::prefix('/products')->group(function () {
-      Route::get('/', [ProductController::class, 'index'])->name('admin.products');
-      Route::post('/', [ProductController::class, 'store'])->name('store.product');
-      Route::put('/{id}', [ProductController::class, 'update'])->name('update.product');
-      Route::delete('/{id}', [ProductController::class, 'destroy'])->name('destroy.product');
+  Route::middleware(['admin'])->group(function () {
+    Route::prefix('/dashboard')->group(function () {
+      Route::get('/', [DashboardController::class, 'getAdminDashboard'])->name('admin.dashboard');
+      Route::get('/orders', [OrderController::class, 'getAdminOrder'])->name('admin.orders');
+  
+      Route::prefix('/products')->group(function () {
+        Route::get('/', [ProductController::class, 'index'])->name('admin.products');
+        Route::post('/', [ProductController::class, 'store'])->name('store.product');
+        Route::put('/{id}', [ProductController::class, 'update'])->name('update.product');
+        Route::delete('/{id}', [ProductController::class, 'destroy'])->name('destroy.product');
+      });
     });
   });
 
+  Route::middleware(['user'])->group(function () {
+      // add User route here
+  });
+
   Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
 });
