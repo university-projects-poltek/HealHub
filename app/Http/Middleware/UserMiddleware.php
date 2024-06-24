8ee77384876e73ae134fae0208550cc7fa6318fb
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-06-24 14:17:16 +0700

    feat: create new middleware for user and admin

--- app/Http/Middleware/UserMiddleware.php
+++ app/Http/Middleware/UserMiddleware.php
@@ -7,24 +7,23 @@ use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
 
-class RoleMiddleware
+class UserMiddleware
 {
   /**
    * Handle an incoming request.
    *
    * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
    */
-  public function handle(Request $request, Closure $next, $roles): Response
+  public function handle(Request $request, Closure $next): Response
   {
     if (!Auth::check()) {
-      return redirect('/not-found'); 
+      return redirect('/login')->with('error', 'Please Sign In first.');
+    } 
+
+    if (Auth::check() && Auth::user()->role === 'user') {
+      return $next($request);
     }
 
-    $user = Auth::user();
-    if (!$user->role == 'admin' && !$user->role == 'user'){
-      return redirect('/not-found');
-    }
-
-    return $next($request);
+    return redirect('/not-found');
   }
 }
