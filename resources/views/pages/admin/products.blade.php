d291a9c14774d5b06a313b2d5fad386a1a44beef
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-06-30 11:16:28 +0700

    feat: create upload image and change quantity to stock on ProductController

--- resources/views/pages/admin/products.blade.php
+++ resources/views/pages/admin/products.blade.php
@@ -1,41 +1,5 @@
 @extends('layouts.dashboard')
 @section('content')
-  @if(session('success'))
-  <div id="toast-success" class="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
-    <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-        </svg>
-        <span class="sr-only">Check icon</span>
-    </div>
-    <div class="ms-3 text-sm font-normal">{{ session('success') }}</div>
-    <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-success" aria-label="Close">
-        <span class="sr-only">Close</span>
-        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-        </svg>
-    </button>
-  </div>
-  @endif
-
-  @if(session('error'))
-  <div id="toast-danger" class="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800" role="alert">
-    <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z"/>
-        </svg>
-        <span class="sr-only">Error icon</span>
-    </div>
-    <div class="ms-3 text-sm font-normal">{{ session('error') }}</div>
-    <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-danger" aria-label="Close">
-        <span class="sr-only">Close</span>
-        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-        </svg>
-    </button>
-</div>
-  @endif
-
   <div class="flex flex-col">
     
     <div class="overflow-x-auto">
@@ -71,7 +35,7 @@
                 Price
               </th>
               <th scope="col" class="px-6 py-3">
-                Quantity
+                Stock
               </th>
               <th scope="col" class="px-6 py-3">
                 Manufacturer
@@ -100,7 +64,7 @@
                 {{ $product->price }}
               </td>
               <td class="px-6 py-4">
-                {{ $product->quantity }}
+                {{ $product->stock }}
               </td>
               <td class="px-6 py-4">
                 {{ $product->manufacturer }}
@@ -123,8 +87,9 @@
                   data-id="{{ $product->id }}"
                   data-name="{{ $product->name }}" 
                   data-price="{{ $product->price}}"
+                  data-image="{{ $product->image }}"
                   data-description="{{ $product->description }}" 
-                  data-quantity="{{ $product->quantity }}"
+                  data-stock="{{ $product->stock }}"
                   data-manufacturer="{{ $product->manufacturer }}"
                 >
                   Update
@@ -161,7 +126,7 @@
         </div>
         <!-- Modal body -->
         <div class="p-4 md:p-5">
-          <form id="form_create" action="{{ route('store.product') }}" method="POST" class="space-y-4">
+          <form id="form_create" action="{{ route('store.product') }}" method="POST" enctype="multipart/form-data" class="space-y-4">
             @csrf 
             @method('POST')
             <div>
@@ -230,20 +195,40 @@
               @endif
             </div>
 
+
+
             <div>
-              <label for="quantity" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                Quantity
+              <label for="image" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Image
+              </label>
+              <input 
+                type="file" 
+                id="image"
+                name="image" 
+                placeholder=""
+                value="{{ old('image') }}"
+                class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" 
+              />
+              @if ($errors->has('image'))
+                <p class="text-red-500 text-xs mt-2">{{ $errors->first('image') }}</p>
+              @endif
+            </div>
+
+
+            <div>
+              <label for="stock" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Stock
               </label>
               <input 
                 type="number" 
-                id="quantity" 
-                name="quantity" 
+                id="stock"
+                name="stock" 
                 placeholder=""
-                value="{{ old('quantity') }}"
+                value="{{ old('stock') }}"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" 
               />
-              @if ($errors->has('quantity'))
-                <p class="text-red-500 text-xs mt-2">{{ $errors->first('quantity') }}</p>
+              @if ($errors->has('stock'))
+                <p class="text-red-500 text-xs mt-2">{{ $errors->first('stock') }}</p>
               @endif
             </div>
 
@@ -301,7 +286,7 @@
         </div>
         <!-- Modal body -->
         <div class="p-4 md:p-5">
-          <form id="form_update" method="POST" class="space-y-4">
+          <form id="form_update" method="POST" enctype="multipart/form-data" class="space-y-4">
             @csrf
             @method('PUT')
             <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
@@ -331,13 +316,25 @@
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" 
             />
 
-            <label for="quantity" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Quantity</label>
+            <label for="image" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Stock</label>
             <input 
               type="number" 
-              id="update_quantity" 
-              name="quantity" 
+              id="update_stock" 
+              name="stock" 
               placeholder=""
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" 
+            />
+
+            <label for="stock" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Image</label>
+              <p id="image_value"></p>
+              <p id="no_file_message">Tidak ada file yang diunggah</p>
+            
+            <input 
+              type="file" 
+              id="update_image" 
+              name="image" 
+              placeholder=""
+              class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" 
             />
 
             <label for="manufacturer" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Manufacturer</label>
@@ -352,7 +349,7 @@
             {{-- Category Input --}}
             <input 
             type="number" id="category" name="category_id" placeholder="Product name"
-              class="inp ut input-bordered w-full max-w-xs" value="3" hidden />
+              class="input input-bordered w-full max-w-xs" value="3" hidden />
 
             <button type="submit"
               class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
@@ -395,34 +392,53 @@
 </div>
 
   <script type="module">
-    $("button#open_update_modal").click(function(){
-      let id =  $(this).data('id')
-      let name =  $(this).data('name')
-      let price =  $(this).data('price')
-      let description =  $(this).data('description')
-      let quantity =  $(this).data('quantity')
-      let manufacturer =  $(this).data('manufacturer')
+    $(document).ready(function () {
 
-      $('#update_name').val(name)
-      $('#update_price').val(price)
-      $('#update_description').val(description)
-      $('#update_quantity').val(quantity)
-      $('#update_manufacturer').val(manufacturer)
+      @if (session('error'))
+        alert("{{ session('error') }}");
+      @endif
 
-      $('#form_update').attr('action', '/dashboard/products/' + id);
+      @if (session('success'))
+        alert("{{ session('success') }}");
+      @endif
 
-      $('#update_modal').trigger('showModal');
-    });
-
-    $("button#open_delete_modal").click(function(){
-      let id = $(this).data('id')
-      let name = $(this).data('name')
-
-      $('span#delete_name').text(name);
-
-      $('#form_delete').attr('action', '/dashboard/products/' + id);
-
-      $('#delete_modal').trigger('showModal');
+      $("button#open_update_modal").click(function(){
+        let id =  $(this).data('id')
+        let name =  $(this).data('name')
+        let price =  $(this).data('price')
+        let description =  $(this).data('description')
+        let image = $(this).data('image')
+        let stock =  $(this).data('stock')
+        let manufacturer =  $(this).data('manufacturer')
+        
+        $('#image_value').text(image)
+        if (!image) {
+          $('#no_file_message').show();
+        } else {
+          $('#no_file_message').hide();
+        }
+  
+        $('#update_name').val(name)
+        $('#update_price').val(price)
+        $('#update_description').val(description)
+        $('#update_stock').val(stock)
+        $('#update_manufacturer').val(manufacturer)
+  
+        $('#form_update').attr('action', '/dashboard/products/' + id);
+  
+        $('#update_modal').trigger('showModal');
+      });
+  
+      $("button#open_delete_modal").click(function(){
+        let id = $(this).data('id')
+        let name = $(this).data('name')
+  
+        $('span#delete_name').text(name);
+  
+        $('#form_delete').attr('action', '/dashboard/products/' + id);
+  
+        $('#delete_modal').trigger('showModal');
+      })
     })
   </script>
 @endsection
