7bbef4063d9fe2d1c6f3fff42fc317b9b6250c88
Haikal Ramadhan <haikalramadhan1015@gmail.com>
2024-07-01 19:26:14 +0700

    feat: create order table on admin side

--- resources/views/pages/admin/order.blade.php
+++ resources/views/pages/admin/order.blade.php
@@ -6,26 +6,46 @@
 @endif
 
 <section>
-  <table>
-    <tr>
-      <th>No</th>
-      <th>Username</th>
-      <th>Payment Method</th>
-      <th>Total amount</th>
-    </tr>
-    @foreach ($orders as $order)
-    <tr>
-      <td>{{ $loop->iteration }}</td>
-      <td>{{ $order->user->username }}</td>
-      <td>{{ $order->payment_method }}</td>
-      <td>{{ $order->total_amount }}</td>
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Order Date
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Customer Name
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Total Amount
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Action
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($orders as $order)
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                            {{ $order->order_date }}
+                        </td>
+                        <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                            {{ $order->user->username }}
+                        </td>
+                        <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                            Rp. {{ number_format($order->total_amount) }}
+                        </td>
+                        <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                            Shipped
+                        </td>
 
 
-
-    </tr>
-
-    @endforeach
-  </table>
+                    </tr>
+                    @endforeach
+            </tbody>
+        </table>
+    </div>
 
 </section>
 
